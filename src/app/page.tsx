aeb9f2dd52c9d4f3f5fc45fb68f1e74bf84a8028
inuldev <inuldev0@gmail.com>
2025-06-03 23:45:02 +0700

    auth setup (better auth)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,64 @@
+"use client";
+
+import { useState } from "react";
+
+import { authClient } from "@/lib/auth-client";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="text-4xl font-bold text-green-500">Halo Dunia</div>;
+  const { data: session } = authClient.useSession();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email(
+      {
+        name,
+        email,
+        password,
+      },
+      {
+        onError: () => {
+          window.alert("terjadi kesalahan");
+        },
+        onSuccess: () => {
+          window.alert("berhasil daftar");
+        },
+      }
+    );
+  };
+
+  if (session) {
+    return (
+      <div className="p-4 flex flex-col gap-y-4">
+        <p>Masuk sebagai {session.user.name}</p>
+        <Button onClick={() => authClient.signOut()}>Keluar</Button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-4 flex flex-col gap-y-4">
+      <Input
+        placeholder="nama"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button onClick={onSubmit}>Daftar</Button>
+    </div>
+  );
 }
