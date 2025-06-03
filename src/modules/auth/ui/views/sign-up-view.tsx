6190c16a06521c783fa8949aba2b559e16a77ef9
inuldev <inuldev0@gmail.com>
2025-06-04 02:54:52 +0700

    authentication ui

---
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -0,0 +1,240 @@
+"use client";
+
+import { z } from "zod";
+import Link from "next/link";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { OctagonAlertIcon } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { authClient } from "@/lib/auth-client";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z
+  .object({
+    name: z.string().min(1, { message: "Nama harus diisi" }),
+    email: z.string().email(),
+    password: z.string().min(1, { message: "Password harus diisi" }),
+    confirmPassword: z
+      .string()
+      .min(1, { message: "Konfirmasi Password harus diisi" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password tidak sesuai",
+    path: ["confirmPassword"],
+  });
+
+export const SignUpView = () => {
+  const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
+  const [pending, setPending] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    setError(null);
+    setPending(true);
+    authClient.signUp.email(
+      {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      },
+      {
+        onSuccess: () => {
+          setPending(false);
+          router.push("/");
+        },
+        onError: ({ error }) => {
+          setPending(false);
+          setError(error.message);
+        },
+      }
+    );
+  };
+
+  return (
+    <div className="flex flex-col gap-5">
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 md:p-8">
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col items-center text-center">
+                  <h1 className="text-2xl font-bold">Mulai Mendaftar</h1>
+                  <p className="text-muted-foreground text-balance">
+                    Buat Akun Anda
+                  </p>
+                </div>
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>
+                          Nama
+                          <FormControl>
+                            <Input
+                              type="text"
+                              placeholder="Masukkan Nama Anda"
+                              {...field}
+                            />
+                          </FormControl>
+                        </FormLabel>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>
+                          Email
+                          <FormControl>
+                            <Input
+                              type="email"
+                              placeholder="Masukkan Email Anda"
+                              {...field}
+                            />
+                          </FormControl>
+                        </FormLabel>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>
+                          Password
+                          <FormControl>
+                            <Input
+                              type="password"
+                              placeholder="*********"
+                              {...field}
+                            />
+                          </FormControl>
+                        </FormLabel>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="confirmPassword"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>
+                          Konfirmasi Password
+                          <FormControl>
+                            <Input
+                              type="password"
+                              placeholder="*********"
+                              {...field}
+                            />
+                          </FormControl>
+                        </FormLabel>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                {!!error && (
+                  <Alert className="bg-destructive/10 border-none">
+                    <OctagonAlertIcon className="h-4 w-4 !text-destructive" />
+                    <AlertTitle>{error}</AlertTitle>
+                  </Alert>
+                )}
+                <Button disabled={pending} type="submit" className="w-full">
+                  Daftar
+                </Button>
+                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                  <span className="bg-card text-muted-foreground relative z-10 px-2">
+                    Atau mendaftar dengan akun
+                  </span>
+                </div>
+                <div className="grid grid-cols-2 gap-2">
+                  <Button
+                    disabled={pending}
+                    variant="outline"
+                    type="button"
+                    className="w-full"
+                  >
+                    <img
+                      src="/google.svg"
+                      alt="Google"
+                      className="h-4 w-4 mr-2"
+                    />
+                    Google
+                  </Button>
+                  <Button
+                    disabled={pending}
+                    variant="outline"
+                    className="w-full"
+                  >
+                    <img
+                      src="/github.svg"
+                      alt="GitHub"
+                      className="h-4 w-4 mr-2"
+                    />
+                    GitHub
+                  </Button>
+                </div>
+                <div className="text-center text-sm">
+                  Sudah Punya Akun?{" "}
+                  <Link
+                    href="/sign-in"
+                    className="underline underline-offset-4"
+                  >
+                    Login
+                  </Link>
+                </div>
+              </div>
+            </form>
+          </Form>
+          <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+            <img src="/logo.svg" alt="Image" className="h-[90px] w-[90px]" />
+            <p className="text-2xl font-semibold text-white">Temu.AI</p>
+          </div>
+        </CardContent>
+      </Card>
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline  *:[a]:underline-offset-4">
+        Dengan klik lanjut, Anda setuju dengan{" "}
+        <a href="#">Syarat dan Ketentuan</a> dan{" "}
+        <a href="#">Kebijakan Privasi</a>
+      </div>
+    </div>
+  );
+};
