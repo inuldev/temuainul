55e0a72e8b966e8ed5cb45f3e22e36fc6b698b8b
inuldev <inuldev0@gmail.com>
2025-06-05 18:49:42 +0000

    agents data table

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -3,14 +3,28 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { useTRPC } from "@/trpc/client";
+import { EmptyState } from "@/components/empty-state";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
+
+import { columns } from "../components/columns";
+import { DataTable } from "../components/data-table";
 
 export const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data} columns={columns} />
+      {data.length === 0 && (
+        <EmptyState
+          title="Belum Ada Agensi"
+          description="Buat agen untuk bergabung dalam rapat Anda. Setiap agen akan mengikuti instruksi Anda dan dapat berinteraksi dengan peserta selama panggilan berlangsung."
+        />
+      )}
+    </div>
+  );
 };
 
 export const AgentsViewLoading = () => {
